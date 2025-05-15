ec4a0f92dc85786d46443e1455b8902d18932b25
Maik <54471120+Maik2409@users.noreply.github.com>
2025-05-15 10:20:10 +0200

    Add files via upload

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,24 +1,22 @@
+package byteme
+
 class UrlMappings {
     static mappings = {
-        // GET-Requests leiten direkt auf die statischen HTMLs weiter
-        "/"               (uri: "/login.html")
-        "/login"          (uri: "/login.html")
-        "/register"       (uri: "/register.html")
-        "/forgot"         (uri: "/forgot.html")
-        "/username"       (uri: "/username.html")
-        "/lobby"          (uri: "/lobby.html")
-        "/game"           (uri: "/game.html")
+        // JSON-Only Endpoints (POST)
+        post "/login"      (controller: "login",          action: "authenticate", parseRequest: true)
+        post "/register"   (controller: "register",       action: "save",         parseRequest: true)
+        post "/forgot"     (controller: "forgotPassword", action: "update",       parseRequest: true)
+        post "/username"   (controller: "user",           action: "setUsername",  parseRequest: true)
 
-        // POST-Endpoints für Form-Submits
-        post "/doLogin"      (controller: "login", action: "authenticate")
-        post "/doRegister"   (controller: "register", action: "save")
-        post "/doForgot"     (controller: "forgotPassword", action: "update")
-        post "/doUsername"   (controller: "user", action: "setUsername")
-        post "/room/create"  (controller: "room", action: "create")
-        post "/room/join"    (controller: "room", action: "join")
-        post "/room/reset"   (controller: "room", action: "reset")
+        post "/room/create"(controller: "room",           action: "create",       parseRequest: true)
+        post "/room/join"  (controller: "room",           action: "join",         parseRequest: true)
+        post "/room/reset" (controller: "room",           action: "reset",        parseRequest: true)
 
-        "500"(view: '/error')
-        "404"(view: '/notFound')
+        post "/chat/list"  (controller: "chat",           action: "list",         parseRequest: true)
+        post "/chat/post"  (controller: "chat",           action: "post",         parseRequest: true)
+
+        // Fehler-Seiten (statisch)
+        "500"(view: "/error.html")
+        "404"(view: "/404.html")
     }
 }
